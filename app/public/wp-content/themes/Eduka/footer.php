fbadd82fe89f974b7c3ae369702307466dc5d262
Bahati Philemon <bahatiphilmon8@gmail.com>
2025-09-04 15:28:54 +0200

    update  header and footer #1

--- app/public/wp-content/themes/Eduka/footer.php
+++ app/public/wp-content/themes/Eduka/footer.php
@@ -3,97 +3,124 @@
 
 
  <!-- footer area -->
-    <footer class="footer-area">
-        <div class="footer-shape">
-            <img src="assets/img/shape/03.png" alt="">
-        </div>
-        <div class="footer-widget">
-            <div class="container">
-                <div class="row footer-widget-wrapper pt-100 pb-70">
-                    <div class="col-md-6 col-lg-4">
-                        <div class="footer-widget-box about-us">
-                            <a href="#" class="footer-logo">
-                                <img src="assets/img/logo/logo-light.png" alt="">
-                            </a>
-                            <p class="mb-3">
-                                We are many variations of passages available but the majority have suffered alteration
-                                in some form by injected humour words believable.
-                            </p>
-                            <ul class="footer-contact">
-                                <li><a href="tel:+21236547898"><i class="far fa-phone"></i>+2 123 654 7898</a></li>
-                                <li><i class="far fa-map-marker-alt"></i>25/B Milford Road, New York</li>
-                                <li><a href="https://live.themewild.com/cdn-cgi/l/email-protection#acc5c2cac3ecc9d4cdc1dcc0c982cfc3c1"><i
-                                            class="far fa-envelope"></i><span class="__cf_email__" data-cfemail="4a23242c250a2f322b273a262f64292527">[email&#160;protected]</span></a></li>
-                            </ul>
-                        </div>
+   <footer class="footer-area">
+    <div class="footer-shape">
+        
+        <img src="<?php echo get_theme_file_uri('/assets/img/shape/03.png'); ?> " alt="logo" />
+    </div>
+    <div class="footer-widget">
+        <div class="container">
+            <div class="row footer-widget-wrapper pt-100 pb-70">
+                <div class="col-md-6 col-lg-4">
+                    <div class="footer-widget-box about-us">
+                        <a href="#" class="footer-logo">
+                            
+                            <img src="<?php echo get_theme_file_uri('assets/img/logo/logo-light.png'); ?> " alt="logo" />
+                        </a>
+                        <p class="mb-3">
+                            <i> L'UBC vise à anticiper et à surmonter les obstacles auxquels les jeunes diplômés sont
+                                souvent confrontés, notamment l'inemploi (sous-emploi). En offrant une éducation
+                                bilingue et en mettant l'accent sur l'expérience professionnelle, l'université prépare
+                                ses étudiants à être compétitifs sur le marché du travail tant au niveau national
+                                qu'international.</i>
+                        </p>
+                        <ul class="footer-contact">
+                            <li><a href="tel:+21236547898"><i class="far fa-phone"></i>+234 973 677 263</a></li>
+                            <li><i class="far fa-map-marker-alt"></i>022, Av. du Plateau, Nguba, Bukavu</li>
+                            <li><a
+                                    href="https://live.themewild.com/cdn-cgi/l/email-protection#acc5c2cac3ecc9d4cdc1dcc0c982cfc3c1"><i
+                                        class="far fa-envelope"></i><span class="__cf_email__"
+                                        data-cfemail="4a23242c250a2f322b273a262f64292527">infos@ubc.edu.cd</span></a>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="col-md-6 col-lg-2">
-                        <div class="footer-widget-box list">
-                            <h4 class="footer-widget-title">Quick Links</h4>
-                            <ul class="footer-list">
-                                <li><a href="#"><i class="fas fa-caret-right"></i> About Us</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> FAQ's</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Testimonials</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Terms Of Service</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Privacy policy</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Update News</a></li>
-                            </ul>
-                        </div>
+                    <div class="nav-right-btn mt-2">
+                        <a href="/#enroll" class="theme-btn"><span class="fal fa-pencil"></span>S'inscrire</a>
                     </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="footer-widget-box list">
-                            <h4 class="footer-widget-title">Our Campus</h4>
-                            <ul class="footer-list">
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Campus Safety</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Student Activities</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Academic Department</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Planning & Administration</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Office Of The Chancellor</a></li>
-                                <li><a href="#"><i class="fas fa-caret-right"></i> Facility Services</a></li>
-                            </ul>
-                        </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="footer-widget-box list">
+                        <h4 class="footer-widget-title">Quick Links</h4>
+                        <ul class="footer-list">
+                            <li><a href="https://ubc.edu.cd/about"><i class="fas fa-caret-right"></i> A propos</a></li>
+                            <li><a href="https://drive.google.com/file/d/1E1ZmDE3hq1Qyon9EFi8g4o-QnSddjneV/view?usp=sharing"
+                                    target="_blank"><i class="fas fa-caret-right"></i> Dossier Inscription</a></li>
+                            <li><a href="https://drive.google.com/file/d/1O7DvdHuyqKnoKhWDyWSHT6y9hfLpNOVW/view?usp=sharing"
+                                    target="_blank"><i class="fas fa-caret-right"></i> Ecole des vacances</a></li>
+                            <li><a href="https://ubc.edu.cd/servicecomm"><i class="fas fa-caret-right"></i> Service à la
+                                    communauté</a></li>
+                            <li><a href="https://ubc.edu.cd/ecole"><i class="fas fa-caret-right"></i> Ecole des
+                                    langues</a></li>
+                            <li><a href="{{Route('bra')}}"><i class="fas fa-caret-right"></i>BRA/Bourse de
+                                    Résilience Académique</a></li>
+                        </ul>
                     </div>
-                    <div class="col-md-6 col-lg-3">
-                        <div class="footer-widget-box list">
-                            <h4 class="footer-widget-title">Newsletter</h4>
-                            <div class="footer-newsletter">
-                                <p>Subscribe Our Newsletter To Get Latest Update And News</p>
-                                <div class="subscribe-form">
-                                    <form action="#">
-                                        <input type="email" class="form-control" placeholder="Your Email">
-                                        <button class="theme-btn" type="submit">
-                                            Subscribe Now <i class="far fa-paper-plane"></i>
-                                        </button>
-                                    </form>
-                                </div>
+                </div>
+                <!-- <div class="col-md-6 col-lg-3">
+                    <div class="footer-widget-box list">
+                        <h4 class="footer-widget-title">Our Campus</h4>
+                        <ul class="footer-list">
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Campus Safety</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Student Activities</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Academic Department</a>
+                            </li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Planning &
+                                    Administration</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Office Of The
+                                    Chancellor</a></li>
+                            <li><a href="#"><i class="fas fa-caret-right"></i> Facility Services</a></li>
+                        </ul>
+                    </div>
+                </div> -->
+                <div class="col-md-6 col-lg-3">
+                    <div class="footer-widget-box list">
+                        <h4 class="footer-widget-title">Newsletter</h4>
+                        <div class="footer-newsletter">
+                            <p>Inscrivez-vous pour recevoir les dernières mises à jour</p>
+                            <div class="subscribe-form">
+                                <form action="#">
+                                    <input type="email" class="form-control" placeholder="votre Email">
+                                    <button class="theme-btn" type="submit">
+                                        S'abonner <i class="far fa-paper-plane"></i>
+                                    </button>
+                                </form>
+
                             </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="copyright">
-            <div class="container">
-                <div class="copyright-wrapper">
-                    <div class="row">
-                        <div class="col-md-6 align-self-center">
-                            <p class="copyright-text">
-                                &copy; Copyright <span id="date"></span> <a href="#"> Eduka </a> All Rights Reserved.
-                            </p>
-                        </div>
-                        <div class="col-md-6 align-self-center">
-                            <ul class="footer-social">
-                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                                <li><a href="#"><i class="fab fa-whatsapp"></i></a></li>
-                                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                            </ul>
-                        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <div class="copyright-wrapper">
+                <div class="row">
+                    <div class="col-md-6 align-self-center">
+                        <p class="copyright-text">
+                            &copy; Copyright <span id="date"></span> <a href="#"> UBC </a> All Rights
+                            Reserved.
+                        </p>
+                    </div>
+                    <div class="col-md-6 align-self-center">
+                        <ul class="footer-social">
+                            <li><a href="https://web.facebook.com/universitebilingueducongo"><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/universit%C3%A9-bilingue-du-congo-277119189/"><i
+                                        class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="tel:+234 973 677 263"><i class="fab fa-whatsapp"></i></a></li>
+                            <li><a href="https://www.youtube.com/@ubcacademy-ci2cs"><i class="fab fa-youtube"></i></a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+
+</footer>
+
     <!-- footer area end -->
 
 
@@ -102,19 +129,7 @@
     <!-- scroll-top end -->
 
 
-    <!-- js -->
-    <!-- <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/jquery-3.7.1.min.js"></script>
-    <script src="assets/js/modernizr.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/isotope.pkgd.min.js"></script>
-    <script src="assets/js/jquery.appear.min.js"></script>
-    <script src="assets/js/jquery.easing.min.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/counter-up.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/main.js"></script> -->
+    
 
     <?php
     wp_footer();
